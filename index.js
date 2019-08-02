354188eacae8b63506d727b960a1305e1e4f172d
Apple <apple@Apples-MacBook-Pro-32.local>
2019-08-02 11:55:05 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var stringVariable = "Hello, I'm some text";
+var numberVariable = 25;
+var numberArray = [16, 13, 13, 15, 28, 16];
+var stringArray = ["Jordan", "Kyra", "Kelsey", "Rachana", "Dani", "Priyanka"];
+var dictionary = {
+  "key": "value",
+  "Jordan": 16,
+  "Kyra": 13,
+  "Kelsey": 13,
+  "Rachana": 15,
+  "Dani": 28,
+  "Priyanka": 16,
+
+}
+
+var dogName = "Tidbit and Buttercup";
+var dogLeash = 1;
+var dogBags = 2;
+var waterBottles = 1;
+
+var numItems = dogLeash + dogBags + waterBottles;
+
+console.log(`Take ${dogName} on a walk! Don't forget to bring ${numItems} items!`)
